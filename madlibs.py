8a7f55ae9bf050a4f39b58405b977617155c9fb0
Ikey Benzaken <ikey.benz@gmail.com>
2018-09-03 23:49:04 -0700

    Added Dope Colors

--- madlibs.py
+++ madlibs.py
@@ -1,7 +1,8 @@
+import colors as c
 import os
 
 def displayOptions():
-    print("Choose what you'd like to do:\n\tP: Play Level\n\tC: Create Level\n\tQ: Quit\n")
+    print(c.blue + "Choose what you'd like to do:" + c.yellow + "\n\tP: Play Level\n\tC: Create Level\n\tQ: Quit\n" + c.end)
     selection = getValidInput('Choose P/C/Q: ', ['P', 'p', 'C', 'c', 'Q', 'q'])
     if selection in 'Pp':
         levelSelect()
@@ -12,19 +13,22 @@ def displayOptions():
     return True
 
 def createLevel():
-    print('Creating new level...\n')
+    print(c.magenta + 'Creating new level...\n')
     variables, story = getMadLib()
+    if not story: 
+        return displayOptions()
     name = getViableName()
     levelFile = open('Levels/' + name + '.txt', 'w')
     levelFile.write(variables + '\n' + story)
     levelFile.close()
 
 def getViableName():
-    name = input('What do you want to call this MadLib? ')
+    name = input(c.blue + 'What do you want to call this MadLib? ' + c.yellow)
     if name not in [level[:-4] for level in os.listdir('Levels')]:
+        print(c.magenta + 'Saving', name + '...\n')
         return name
     else:
-        print('The name you have chosen for your level is already in use by a different level.')
+        print(c.red + 'The name you have chosen for your level is already in use by a different level.')
     return getViableName()
 
 def playLevel(levelName):
@@ -32,30 +36,27 @@ def playLevel(levelName):
     variables = levelFile.readline()[:-1].split(' ')
     story = levelFile.readline()
     if getValidInput('Do you want to see the story before filling it in? (y/n) ', ['Y', 'y', 'N', 'n']) in 'Yy':
-        print(colorify(story, variables))
-    print('\nCompleted MadLib:\n' + fillOutMadLib(story, variables) + '\n')
+        print('\n' + c.blue + levelName + ':\n   ' + c.gray + colorify(story, variables) + '\n')
+    print(c.blue + '\nCompleted MadLib:   \n' + fillOutMadLib(story, variables) + '\n')
 
 def levelSelect():
-    if not os.path.isdir('Levels'):
-        os.mkdir('Levels')
+    print(c.magenta + 'Loading Saved Levels...\n')
     options = [level[:-4] for level in os.listdir('Levels')]
     if len(options) > 0:
-        print('Levels:\n' + ''.join(['\t' + str(index + 1) + '. ' + level + '\n' for index, level in enumerate(options)]))
+        print(c.blue + 'Levels:\n' + c.yellow + ''.join(['\t' + str(index + 1) + '. ' + level + '\n' for index, level in enumerate(options)]))
         chosenLevel = getValidInput("Enter the level name you'd like to play: ", options)
         playLevel(chosenLevel)
     else:
-        print('There are no levels to play yet.')
+        print(c.red + 'There are no levels to play yet.\n')
         if getValidInput('Want to create one? (y/n): ', ['Y', 'y', 'N', 'n']) in 'Yy':
             createLevel()
 
 def getMadLib():
-    keepAsking = True
-    while keepAsking:
-        variables = input('Enter the variable names required for this level separated by spaces: ')
-        story = input("Enter your story separated by spaces, enter " + variables + " for places that require user input: ")
-        print('Your MadLib:', colorify(story, variables.split(' ')))
-        if getValidInput('Save? (y/n): ', ['Y', 'y', 'N', 'n']) in 'Yy':
-            keepAsking = False
+    variables = input(c.blue + 'Enter the variable names required for this level separated by spaces: ' + c.yellow)
+    story = input(c.blue + "Enter your story separated by spaces, enter " + c.cyan + variables + c.blue + " for places that require user input: " + c.gray)
+    print(c.blue + 'Your MadLib:', colorify(story, variables.split(' ')), '\n')
+    if getValidInput('Save? (y/n): ', ['Y', 'y', 'N', 'n']) in 'Yy':
+        return False, False
     return variables, story
     
 def fillOutMadLib(story, variables):
@@ -65,36 +66,38 @@ def fillOutMadLib(story, variables):
         newWord = word
         if word in variables:
             if word[-1] == ".":
-                newWord = input("Enter a " + word[:-1] + ": ") + '.'
+                newWord = input(c.blue + "Enter a "+ c.cyan + word[:-1] + c.blue + ": " + c.yellow) + '.'
             else:
-                newWord = input('Enter a ' + word + ': ')
+                newWord = input(c.blue + 'Enter a '+ c.cyan + word + c.blue + ": " + c.yellow)
         newWords.append(newWord)
-    return " ".join(newWords)
+    return c.gray + " ".join(newWords)
 
 def getValidInput(prompt, validResponses):
-    response = input(prompt)
+    response = input(c.blue + prompt + c.yellow)
     if response not in validResponses:
-        print('That is an invalid response.\n')
+        print(c.red + 'That is an invalid response.' + c.end + '\n')
         return getValidInput(prompt, validResponses)
     return response
 
 def colorify(wordString, specials):
-    newWords = []
+    newWords = [c.gray]
     for word in wordString.split(' '):
         newWord = word
         withPeriod = word[-1] == '.'
         if withPeriod: word = word[:-1]
         if word in specials:
-            newWord = '\033[0;36;40m' + word + '\033[0m'
+            newWord = c.cyan + word + c.gray
         if withPeriod: newWord += '.'
         newWords.append(newWord)
     return " ".join(newWords)
 
 def main():
-    print('--- Mad Libs ---\n')
+    print(c.blue + '--- Mad Libs ---\n')
+    if not os.path.isdir('Levels'):
+        os.mkdir('Levels')
     keepGoing = displayOptions()
     while keepGoing:
         keepGoing = displayOptions()
-    print('\nThanks for playing!!!!')
+    print(c.magenta + '\nThanks for playing!!!!\n' + c.end)
     
 main()
